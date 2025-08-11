0ab2d32520f540a7f574e31ff09625256f8383d3
ItsAviel <avielcohen259@gmail.com>
2025-08-11 21:00:24 +0300

    fix main class

--- src/main/java/info/debatty/java/stringsimilarity/examples/Examples.java
+++ src/main/java/info/debatty/java/stringsimilarity/examples/Examples.java
@@ -1,230 +1,83 @@
-/*
- * The MIT License
- *
- * Copyright 2015 Thibault Debatty.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
 package info.debatty.java.stringsimilarity.examples;
 
 import info.debatty.java.stringsimilarity.*;
 
-/**
- * @author Thibault Debatty
- */
 public class Examples {
+
     public static void main(String[] args) {
 
-        //Levenshtein with traceback
-
-        System.out.println("\n\n================== Levenshtein with traceback :==================\nA=Moti, B=Schneider");
-        System.out.println("Distance: ");
+        // ===== Levenshtein with Traceback =====
         String s1 = "Schneider";
         String s2 = "Moti";
-        LevenshteinV2.Result r = LevenshteinV2.compute(s1, s2);
-        LevenshteinV2.printMatrix(r.dp, s1, s2);
-        System.out.println(r.alignedA);
-        System.out.println(r.alignedB);
-        System.out.println("Distance: " + r.distance);
 
+        System.out.println("\n\n============ Levenshtein with Traceback ============");
+        System.out.println("A = " + s1 + ", B = " + s2);
 
+        LevenshteinV2.Result result = LevenshteinV2.compute(s1, s2);
 
-        // Normalized Levenshtein
-        // ======================
+        System.out.println("\n** The matrix is: **");
+        LevenshteinV2.printMatrix(result.dp, s1, s2);
 
-//        NormalizedLevenshtein norLevenshtein = new NormalizedLevenshtein();
-//        Levenshtein levenshtein = new Levenshtein();
-//        System.out.println("\n\n================== Example:==================\nA=Moti, B=Schneider");
-//        System.out.println("Distance: ");
-//        System.out.println(levenshtein.distance("Moti", "Schneider"));
-//        System.out.println("Normalized distance: ");
-//        System.out.println(norLevenshtein.distance("Moti", "Schneider"));
-//        System.out.println("Similarity:");
-//        System.out.println(norLevenshtein.similarity("Moti", "Schneider"));
-//
+        System.out.println("\n** Traceback **");
+        System.out.println(result.alignedA);
+        System.out.println(result.alignedB);
 
-//        // Levenshtein
-//        // ===========
-//        System.out.println("\nLevenshtein");
-//        Levenshtein levenshtein = new Levenshtein();
-//        System.out.println("Moti Sheneider");
-//        System.out.println(levenshtein.distance("Moti",  "Schneider"));
+        System.out.println("\nDistance: " + result.distance);
+        NormalizedLevenshtein normalized = new NormalizedLevenshtein();
+        System.out.println("Similarity: " + normalized.similarity(s1, s2));
 
-
-//        // Jaccard index
-//        // =============
-//        System.out.println("\nJaccard");
-//        Jaccard j2 = new Jaccard(2);
-//        // AB BC CD DE DF
-//        // 1  1  1  1  0
-//        // 1  1  1  0  1
-//        // => 3 / 5 = 0.6
-//        System.out.println(j2.similarity("ABCDE", "ABCDF"));
-//
-//        // Jaro-Winkler
-//        // ============
-//        System.out.println("\nJaro-Winkler");
-//        JaroWinkler jw = new JaroWinkler();
-//
-//        // substitution of s and t : 0.9740740656852722
-//        System.out.println(jw.similarity("My string", "My tsring"));
-//
-//        // substitution of s and n : 0.8962963223457336
-//        System.out.println(jw.similarity("My string", "My ntrisg"));
-//
-//        // Cosine
-//        // ======
-//        System.out.println("\nCosine");
-//        Cosine cos = new Cosine(3);
-//
-//        // ABC BCE
-//        // 1  0
-//        // 1  1
-//        // angle = 45°
-//        // => similarity = .71
-//        System.out.println(cos.similarity("ABC", "ABCE"));
-//
-//        cos = new Cosine(2);
-//        // AB BA
-//        // 2  1
-//        // 1  1
-//        // similarity = .95
-//        System.out.println(cos.similarity("ABAB", "BAB"));
-//
-//        // Damerau
-//        // =======
-//        System.out.println("\nDamerau");
-//        Damerau damerau = new Damerau();
-//
-//        // 1 substitution
-//        System.out.println(damerau.distance("ABCDEF", "ABDCEF"));
-//
-//        // 2 substitutions
-//        System.out.println(damerau.distance("ABCDEF", "BACDFE"));
-//
-//        // 1 deletion
-//        System.out.println(damerau.distance("ABCDEF", "ABCDE"));
-//        System.out.println(damerau.distance("ABCDEF", "BCDEF"));
-//
-//        System.out.println(damerau.distance("ABCDEF", "ABCGDEF"));
-//
-//        // All different
-//        System.out.println(damerau.distance("ABCDEF", "POIU"));
-//
-//
-//        // Optimal String Alignment
-//        // =======
-//        System.out.println("\nOptimal String Alignment");
-//        OptimalStringAlignment osa = new OptimalStringAlignment();
-//
-//        //Will produce 3.0
-//        System.out.println(osa.distance("CA", "ABC"));
-//
-//
-//        // Longest Common Subsequence
-//        // ==========================
-//        System.out.println("\nLongest Common Subsequence");
-//        LongestCommonSubsequence lcs = new LongestCommonSubsequence();
-//
-//        // Will produce 4.0
-//        System.out.println(lcs.distance("AGCAT", "GAC"));
-//
-//        // Will produce 1.0
-//        System.out.println(lcs.distance("AGCAT", "AGCT"));
-//
-//        // NGram
-//        // =====
-//        // produces 0.416666
-//        System.out.println("\nNGram");
-//        NGram twogram = new NGram(2);
-//        System.out.println(twogram.distance("ABCD", "ABTUIO"));
-//
-//        // produces 0.97222
-//        String s1 = "Adobe CreativeSuite 5 Master Collection from cheap 4zp";
-//        String s2 = "Adobe CreativeSuite 5 Master Collection from cheap d1x";
-//        NGram ngram = new NGram(4);
-//        System.out.println(ngram.distance(s1, s2));
-
-
-//
-//        // QGram
-//        // =====
-//        System.out.println("\nQGram");
-//        QGram dig = new QGram(2);
-//
-//        // AB BC CD CE
-//        // 1  1  1  0
-//        // 1  1  0  1
-//        // Total: 2
-//        System.out.println(dig.distance("ABCD", "ABCE"));
-//
-//        System.out.println(dig.distance("", "QSDFGHJKLM"));
-//
-//        System.out.println(dig.distance(
-//                "Best Deal Ever! Viagra50/100mg - $1.85 071",
-//                "Best Deal Ever! Viagra50/100mg - $1.85 7z3"));
-//
-//        // Sorensen-Dice
-//        // =============
-//        System.out.println("\nSorensen-Dice");
-//        SorensenDice sd = new SorensenDice(2);
-//
-//        // AB BC CD DE DF FG
-//        // 1  1  1  1  0  0
-//        // 1  1  1  0  1  1
-//        // => 2 x 3 / (4 + 5) = 6/9 = 0.6666
-//        System.out.println(sd.similarity("ABCDE", "ABCDFG"));
-//
-//        // Weighted Levenshtein
-//        // ====================
-//        System.out.println("\nWeighted Levenshtein");
-//        WeightedLevenshtein wl = new WeightedLevenshtein(
-//                new CharacterSubstitutionInterface() {
-//                    public double cost(char c1, char c2) {
-//
-//                        // The cost for substituting 't' and 'r' is considered
-//                        // smaller as these 2 are located next to each other
-//                        // on a keyboard
-//                        if (c1 == 't' && c2 == 'r') {
-//                            return 0.5;
-//                        }
-//
-//                        // For most cases, the cost of substituting 2 characters
-//                        // is 1.0
-//                        return 1.0;
-//                    }
-//                });
-//
-//        System.out.println(wl.distance("String1", "Srring2"));
-//
-//        // K-Shingling
-//        System.out.println("\nK-Shingling");
-//        s1 = "my string,  \n  my song";
-//        s2 = "another string, from a song";
-//        Cosine cosine = new Cosine(4);
-//        System.out.println(cosine.getProfile(s1));
-//        System.out.println(cosine.getProfile(s2));
-//
-//        cosine = new Cosine(2);
-//        System.out.println(cosine.getProfile("ABCAB"));
-
+        /*
+         * ===== More examples =====
+         *
+         * // Normalized Levenshtein
+         * Levenshtein levenshtein = new Levenshtein();
+         * System.out.println(levenshtein.distance(s1, s2));
+         * System.out.println(normalized.distance(s1, s2));
+         *
+         * // Jaccard
+         * Jaccard j2 = new Jaccard(2);
+         * System.out.println(j2.similarity("ABCDE", "ABCDF"));
+         *
+         * // Jaro-Winkler
+         * JaroWinkler jw = new JaroWinkler();
+         * System.out.println(jw.similarity("My string", "My tsring"));
+         *
+         * // Cosine
+         * Cosine cos = new Cosine(3);
+         * System.out.println(cos.similarity("ABC", "ABCE"));
+         *
+         * // Damerau
+         * Damerau damerau = new Damerau();
+         * System.out.println(damerau.distance("ABCDEF", "ABDCEF"));
+         *
+         * // Optimal String Alignment
+         * OptimalStringAlignment osa = new OptimalStringAlignment();
+         * System.out.println(osa.distance("CA", "ABC"));
+         *
+         * // Longest Common Subsequence
+         * LongestCommonSubsequence lcs = new LongestCommonSubsequence();
+         * System.out.println(lcs.distance("AGCAT", "GAC"));
+         *
+         * // NGram
+         * NGram twogram = new NGram(2);
+         * System.out.println(twogram.distance("ABCD", "ABTUIO"));
+         *
+         * // QGram
+         * QGram dig = new QGram(2);
+         * System.out.println(dig.distance("ABCD", "ABCE"));
+         *
+         * // Sorensen-Dice
+         * SorensenDice sd = new SorensenDice(2);
+         * System.out.println(sd.similarity("ABCDE", "ABCDFG"));
+         *
+         * // Weighted Levenshtein
+         * WeightedLevenshtein wl = new WeightedLevenshtein((c1, c2) -> {
+         *     if (c1 == 't' && c2 == 'r') {
+         *         return 0.5;
+         *     }
+         *     return 1.0;
+         * });
+         * System.out.println(wl.distance("String1", "Srring2"));
+         */
     }
 }
-
-
